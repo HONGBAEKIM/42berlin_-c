89e7c621a650db61bdfafe28bf155efd44796a53
Hongbae Kim <hongbaki@c4c1c3.42berlin.de>
2024-05-15 20:49:53 +0200

    added comments

--- exam06/mini_serv.c
+++ exam06/mini_serv.c
@@ -8,6 +8,7 @@
 // 65536
 
 int		count = 0, max_fd = 0;
+//to store identifiers for clients connected to the server
 int		ids[65536];
 char	*msgs[65536];
 
@@ -81,14 +82,18 @@ void	fatal_error()
 	exit(1);
 }
 
+//to inform all other clients that a new client has arrived
 void	notify_other(int author, char *str) 
 {
 	for (int fd = 0; fd <= max_fd; fd++) 
 	{
+		// To determine if a client socket is ready to receive a message.
+		// and if it's not the same as the author (the client that triggered the event)
 		if (FD_ISSET(fd, &wfds) && fd != author)
 			send(fd, str, strlen(str), 0);
 	}
 }
+
 
 void	register_client(int fd) 
 {
@@ -96,6 +101,7 @@ void	register_client(int fd)
 	ids[fd] = count++;
 	msgs[fd] = NULL;
 	FD_SET(fd, &afds);
+	// Format and store values("server: client %d just arrived\n") into the buf_write
 	sprintf(buf_write, "server: client %d just arrived\n", ids[fd]);
 	notify_other(fd, buf_write);
 }
@@ -113,11 +119,15 @@ void	send_msg(int fd)
 {
 	char *msg;
 
+	// Continue extracting messages until there are no more messages
 	while (extract_message(&(msgs[fd]), &msg)) 
 	{
+		// Format the message with client identifier
 		sprintf(buf_write, "client %d: ", ids[fd]);
-		notify_other(fd, buf_write);
-		notify_other(fd, msg);
+		// Notify other clients about the sender and the message
+		notify_other(fd, buf_write); // Notify about the sender
+		notify_other(fd, msg); // Notify about the message
+		// Free the memory allocated for the extracted message
 		free(msg);
 	}
 }
@@ -145,28 +155,35 @@ int		main(int ac, char **av)
 	int sockfd = create_socket();
 
 	struct sockaddr_in servaddr;
+	// socket create and verification 
 	bzero(&servaddr, sizeof(servaddr));
 
+	// assign IP, PORT 
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(2130706433);
 	servaddr.sin_port = htons(atoi(av[1])); // replace 8080
 
+	// Binding newly created socket to given IP and verification 
 	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)))
 		fatal_error();
 	if (listen(sockfd, SOMAXCONN)) // the main uses 10, SOMAXCONN is 180 on my machine
 		fatal_error();
 
-	while (1) {
+	while (1) 
+	{
 		rfds = wfds = afds;
-
+		//to monitor multiple file descriptors for readiness to perform I/O operations
 		if (select(max_fd + 1, &rfds, &wfds, NULL, NULL) < 0)
 			fatal_error();
 
-		for (int fd = 0; fd <= max_fd; fd++) {
+		for (int fd = 0; fd <= max_fd; fd++) 
+		{
+			//to test if a file descriptor is still present in a set
 			if (!FD_ISSET(fd, &rfds))
 				continue;
 
-			if (fd == sockfd) {
+			if (fd == sockfd) 
+			{
 				socklen_t addr_len = sizeof(servaddr);
 				int client_fd = accept(sockfd, (struct sockaddr *)&servaddr, &addr_len);
 				if (client_fd >= 0) {
@@ -174,7 +191,9 @@ int		main(int ac, char **av)
 					break ;
 				}
 			}
-			else {
+			else 
+			{
+				//If no data is available or an error occurs, recv() returns -1
 				int read_bytes = recv(fd, buf_read, 1000, 0);
 				if (read_bytes <= 0) {
 					remove_client(fd);
@@ -188,3 +207,4 @@ int		main(int ac, char **av)
 	}
 	return 0;
 }
+
